1cebd08a9f4d5c1fefe2b9d6077aeccfbf226984
NarekDallakyan <it.narek.dallakyan@gmail.com>
2025-05-03 00:28:31 +0400

    Fixed pointerInput

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -43,7 +43,7 @@ dependencies {
 afterEvaluate {
     publishing {
         publications {
-            create<MavenPublication>("release") {
+            create<MavenPublication>("maven") {
                 from(components["release"])
                 groupId = "com.github.ItNarekDallakyan"
                 artifactId = "ComposeDragAndDrop"
