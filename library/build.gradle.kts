42d74fee6aea26fcf855c211282bc418a65a971c
NarekDallakyan <it.narek.dallakyan@gmail.com>
2025-05-02 23:33:50 +0400

    Fixed pointerInput

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    alias(libs.plugins.android.library)
+    id("com.android.library")
     id("maven-publish")
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
@@ -34,29 +34,65 @@ dependencies {
     implementation(libs.androidx.lifecycle.compose)
 }
 
-publishing {
-    publications {
-        create<MavenPublication>("maven") {
-            groupId = "com.github.ItNarekDallakyan"
-            artifactId = "ComposeDragAndDrop"
-            version = "1.0.0"
+afterEvaluate {
+    publishing {
+        publications {
+            create<MavenPublication>("maven") {
+                from(components["release"])
+                groupId = "com.github.ItNarekDallakyan"
+                artifactId = "ComposeDragAndDrop"
+                version = "1.0.0"
 
-            // Optionally, configure POM metadata if needed
-            pom {
-                name.set("ComposeDragAndDrop")
-                description.set("A library for drag and drop in Compose.")
-                url.set("https://github.com/ItNarekDallakyan/ComposeDragAndDrop")
-                licenses {
-                    license {
-                        name.set("MIT License")
-                        url.set("https://opensource.org/licenses/MIT")
+                // Optionally, configure POM metadata if needed
+                pom {
+                    name.set("ComposeDragAndDrop")
+                    description.set("A library for drag and drop in Compose.")
+                    url.set("https://github.com/ItNarekDallakyan/ComposeDragAndDrop")
+                    licenses {
+                        license {
+                            name.set("MIT License")
+                            url.set("https://opensource.org/licenses/MIT")
+                        }
+                    }
+                    developers {
+                        developer {
+                            id.set("narek")
+                            name.set("Narek Dallakyan")
+                            email.set("it.narek.dallakyan@gmail.com")
+                        }
                     }
                 }
-                developers {
-                    developer {
-                        id.set("Narek")
-                        name.set("Narek Dallakyan")
-                        email.set("it.narek.dallakyan@gmail.com")
+            }
+        }
+    }
+
+    afterEvaluate {
+        publishing {
+            publications {
+                create<MavenPublication>("release") {
+                    from(components["release"])
+                    groupId = "com.github.ItNarekDallakyan"
+                    artifactId = "ComposeDragAndDrop"
+                    version = "1.0.0"
+
+                    // Optionally, configure POM metadata if needed
+                    pom {
+                        name.set("ComposeDragAndDrop")
+                        description.set("A library for drag and drop in Compose.")
+                        url.set("https://github.com/ItNarekDallakyan/ComposeDragAndDrop")
+                        licenses {
+                            license {
+                                name.set("MIT License")
+                                url.set("https://opensource.org/licenses/MIT")
+                            }
+                        }
+                        developers {
+                            developer {
+                                id.set("narek")
+                                name.set("Narek Dallakyan")
+                                email.set("it.narek.dallakyan@gmail.com")
+                            }
+                        }
                     }
                 }
             }
